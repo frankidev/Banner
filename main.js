b60269d6b323e8fec9f03d483c8a90eeddffa332
Nicolás <cannibal5033@gmail.com>
2020-10-20 13:33:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,13 +8,12 @@ imgArray[2].src = 'data/3.jpg';
 
 (function nextImage(element = 'img') {
     var img = document.getElementById(element);
-
-        SetTime(imgArray[1].src, 1500);
-        SetTime(imgArray[2].src, 3000);
-        SetTime(imgArray[0].src, 4500);
-        SetTime(imgArray[1].src, 6000);
-        SetTime(imgArray[2].src, 7500);
-        SetTime(imgArray[0].src, 9000);
+    SetTime(imgArray[1].src, 1500);
+    SetTime(imgArray[2].src, 3000);
+    SetTime(imgArray[0].src, 4500);
+    SetTime(imgArray[1].src, 6000);
+    SetTime(imgArray[2].src, 7500);
+    SetTime(imgArray[0].src, 9000);
 
         return;
 })();
